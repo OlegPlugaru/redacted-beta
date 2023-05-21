35e31b02160b158ef3b82783b4f2465e2c46c622
Oleg Plugaru <oleg.plugaru.md@gmail.com>
2023-05-21 17:24:39 +0300

    Initial Commit

---
+++ redacted-beta/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  redacted-beta
+//
+//  Created by Whoami on 21.05.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ redacted-beta/redacted_betaApp.swift
@@ -0,0 +1,17 @@
+//
+//  redacted_betaApp.swift
+//  redacted-beta
+//
+//  Created by Whoami on 21.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct redacted_betaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
