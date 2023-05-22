55e22302dbcfb728e2f112c52571846d1d897e55
Oleg Plugaru <oleg.plugaru.md@gmail.com>
2023-05-22 20:39:32 +0300

    finished

--- redacted-beta/ContentView.swift
+++ redacted-beta/ContentView.swift
@@ -8,14 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isLoading = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            VStack {
+                Image("Photo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 120, height: 120)
+                    .clipShape(Circle())
+                
+                Text("Oleg Plugaru")
+                    .font(.title)
+                    .bold()
+            }
+            
+            Spacer().frame(height: 30)
+            
+            VStack(alignment: .leading, spacing: 12) {
+                HStack {
+                    Image(systemName: "envelope")
+                        .unredacted()
+                    Text("oleg.plugaru@midnight.works")
+                }
+                
+                HStack {
+                    Image(systemName: "phone")
+                        .unredacted()
+                    Text("+373 786 481 68")
+                }
+                
+                HStack {
+                    Image(systemName: "network")
+                        .unredacted()
+                    Text("olegplugaru.com")
+                }
+            }
+            Spacer().frame(height: 30)
+            
+            Button {
+                print("Button Tapped")
+            } label: {
+                Text("Update Profile")
+                    .bold()
+                    .frame(width: 260, height: 50)
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(12)
+            }
+            .disabled(isLoading)
         }
-        .padding()
+        .onAppear { startNetworkCall() }
+        .redacted(reason: isLoading ? .placeholder : [])
+    }
+    func startNetworkCall() {
+        isLoading = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            isLoading = false
+        }
     }
 }
 
